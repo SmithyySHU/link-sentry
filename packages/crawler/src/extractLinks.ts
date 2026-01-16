bb5f728f8d132c97df52d9c0806ec13882bad08d
SmithyySHU <iloveashleigh37@gmail.com>
2026-01-16 23:00:49 +0000

    ci (fix) attempt fix

--- packages/crawler/src/extractLinks.ts
+++ packages/crawler/src/extractLinks.ts
@@ -1,9 +1,10 @@
 import * as cheerio from "cheerio";
+import type { AnyNode } from "domhandler";
 
 export default function extractLinks(html: string): string[] {
   const $ = cheerio.load(html);
   const links: string[] = [];
-  $("a").each((_, element) => {
+  $("a").each((_: number, element: AnyNode) => {
     const link = $(element).attr("href");
     if (link) {
       links.push(link);
